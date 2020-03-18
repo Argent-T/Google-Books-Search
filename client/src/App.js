0defc81dc3e62139b1c586359c83f91f1f491373
JesterPanda <55069246+Jesterpanda@users.noreply.github.com>
2020-03-18 17:52:54 -0400

    update

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Nav from "./components/Nav";
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+      <Nav />
+      </div>   
+    </Router>
+  );
+}
+
+
+export default App;
